2c4c2ad5d75d5427f40beb7567f281a64a7960c4
Ole Tietz <oletietz@protonmail.com>
2021-01-04 08:47:28 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -3,26 +3,39 @@
 import pyautogui
 from time import sleep
 
+print("Tietzers itslearning Bot")
+print("-------------------------")
+
+screenResX = int(input("Bildschirmauflösung X (X*Y):"))
+screenResY = int(input("Bildschirmauflösung Y (X*Y):"))
+timeout = int(input("Wie viele Sekunden möchtest du in jedem Kurs verbringen?"))
+
+print("\n\nBitte Fenster minimiren und Browser maximieren")
+print("Start in 5 Sekunden")
+sleep(5)
 
 def getX(x):
-    return 320 + (x*310) - 310
+    x1 = 320 + (x*310) - 310
+    x2 = x1 / screenResX
+    return x2 * screenResX
 
 
 def getY(y):
-    return 446 + (y*310) - 310
-
+    y1 = 446 + (y*310) - 310
+    y2 = y1 / screenResY
+    return y2 * screenResY
 
 def clickCourse(x,y):
     pyautogui.moveTo(getX(x), getY(y))
     pyautogui.click()
 
 def goStart():
-    pyautogui.moveTo(30,100)
+    pyautogui.moveTo(0.016625*screenResX,0.1029518*screenResY)
     pyautogui.click()
 
 def scrolldown():
-    pyautogui.moveTo(1913,218)
-    pyautogui.dragRel(0,400,duration=1)
+    pyautogui.moveTo(0.9963541*screenResX,0.201851851*screenResY)
+    pyautogui.dragRel(0,0.37037037*screenResY,duration=1)
 
 print(pyautogui.position())
 
@@ -30,56 +43,59 @@ print(pyautogui.position())
 
 goStart()
 sleep(2)
+print("Click")
 clickCourse(1,1)
-sleep(5)
+sleep(timeout)
 
 goStart()
 sleep(2)
+print("Click")
 clickCourse(2, 1)
-sleep(5)
+sleep(timeout)
 
 goStart()
 sleep(2)
 clickCourse(3,1)
-sleep(5)
+sleep(timeout)
 
 goStart()
 sleep(2)
 clickCourse(4,1)
-sleep(5)
+sleep(timeout)
 
 goStart()
 sleep(2)
 clickCourse(5,1)
-sleep(5)
+sleep(timeout)
 goStart()
 sleep(2)
 
 goStart()
 sleep(2)
 clickCourse(1,2)
-sleep(5)
+sleep(timeout)
 
 goStart()
 sleep(2)
 clickCourse(2,2)
-sleep(5)
+sleep(timeout)
 
 goStart()
 sleep(2)
 clickCourse(3,2)
-sleep(5)
+sleep(timeout)
 
 goStart()
 sleep(2)
 clickCourse(4,2)
-sleep(5)
+sleep(timeout)
 
 goStart()
 sleep(2)
 clickCourse(5,2)
-sleep(5)
+sleep(timeout)
 
 goStart()
 sleep(2)
 scrolldown()
+
